e7f4c093a4a5f0d11224423f7ec093dc80f417c0
Ishotihadus <hanachan.pao@gmail.com>
2022-06-12 17:31:58 +0900

    add retry to Google::Cloud::Stroge::Bucket#f

--- lib/google/cloud/storage/extension.rb
+++ lib/google/cloud/storage/extension.rb
@@ -13,14 +13,33 @@ module Google
         # @yield [path]
         # @yieldparam path [String] path of tempfile
         def f(filename, mode: 0)
-          file = self.file(filename)
+          tries = 0
+          begin
+            file = self.file(filename)
+          rescue StandardError
+            tries += 1
+            retry if tries <= 5
+
+            raise
+          end
           raise 'file not found' unless file
 
           name = file.name
           basename = ::File.basename(name, '.*')
           extname = ::File.extname(name)
           tempfile = ::Tempfile.create([basename, extname], mode:)
-          file.download(tempfile.path)
+
+          tries = 0
+          begin
+            file.download(tempfile.path)
+          rescue StandardError
+            tries += 1
+            retry if tries <= 5
+
+            ::File.delete(tempfile.path)
+            raise
+          end
+
           ret = yield(tempfile.path)
           ::File.delete(tempfile.path)
 
